c710a53eee632e7b7b2a255ff122b3f0fcff5e08
Squall <dungnc1607@gmail.com>
2018-10-17 10:15:26 +0700

    make Register screen prototype

--- MoneyStack/MoneyStack/View/Register/MSRegisterVC.swift
+++ MoneyStack/MoneyStack/View/Register/MSRegisterVC.swift
@@ -10,26 +10,37 @@ import UIKit
 
 class MSRegisterVC: MSBaseVC {
 
+    @IBOutlet weak var textfieldEmail: UITextField!
+    @IBOutlet weak var textfieldUserName: UITextField!
+    @IBOutlet weak var textfieldFullName: UITextField!
+    @IBOutlet weak var textfieldJob: UITextField!
+    @IBOutlet weak var textfieldKindOfUser: UITextField!
+    @IBOutlet weak var textfieldPassword: UITextField!
+    @IBOutlet weak var textfieldRepeatPassword: UITextField!
+    @IBOutlet weak var textfieldGender: UITextField!
+    @IBOutlet weak var textfieldBirthday: UITextField!
+    
+    @IBOutlet weak var imageBack: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imageBack.image = imageBack.image!.withRenderingMode(.alwaysTemplate)
+        imageBack.tintColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func actionPressBack(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func actionPressRegister(_ sender: Any) {
+    }
+    
+    @IBAction func actionPressQuestionMark(_ sender: Any) {
+    }
+    
+    
+    
 }
